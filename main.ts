bc8cabe6e4c156f30b5e0ea418d6c8198e29566c
1775779 <79492954+1775779@users.noreply.github.com>
2021-02-23 12:55:38 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showString("Chloe Wirt")
+})
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+})
